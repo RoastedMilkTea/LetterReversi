39275d44a4e7b8379e09d0175c51306a0443d012
Jessie Z. <85249443+RoastedMilkTea@users.noreply.github.com>
2024-08-01 23:21:10 -0400

    Update lab8.h

--- lab8.h
+++ lab8.h
@@ -1,7 +1,7 @@
 #ifndef REVERSI_H
 #define REVERSI_H
 #include <stdbool.h>
-// Function prototypes for Lab 7
+
 void printBoard(char board[][26], int n);
 bool positionInBounds(int n, int row, int col);
 bool checkLegalInDirection(char board[][26], int n, int row, int col,
